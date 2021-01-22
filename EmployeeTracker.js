0b02df2aa4cc6d1ff4ee5ef9b8515a6054f69126
Ilya Libershteyn <ilya.v.libershteyn@gmail.com>
2021-01-21 23:23:00 -0800

    implementing db code

--- EmployeeTracker.js
+++ EmployeeTracker.js
@@ -1,7 +1,8 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-var connection = mysql.createConnection({
+var connection = mysql.createConnection
+({
   host: "localhost",
 
   // Your port; if not 3306
@@ -12,90 +13,119 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "",
-  database: "top_songsDB"
+  database: "employeeDB"
 });
 
-connection.connect(function(err) {
+connection.connect(function(err) 
+{
   if (err) throw err;
   runSearch();
 });
 
-function runSearch() {
+function runSearch() 
+{
   inquirer
-    .prompt({
+    .prompt
+    ({
       name: "action",
       type: "rawlist",
       message: "What would you like to do?",
       choices: [
-        "Find songs by artist",
-        "Find all artists who appear more than once",
-        "Find data within a specific range",
-        "Search for a specific song",
-        "Find artists with a top song and top album in the same year"
+        "Add departments",
+        "Add roles",
+        "Add employees",
+        "View employees",
+        "View roles",
+        "View departments",
+        "Update employee roles"
       ]
     })
-    .then(function(answer) {
-      switch (answer.action) {
-      case "Find songs by artist":
-        artistSearch();
-        break;
+    .then(function(answer) 
+    {
+      switch (answer.action) 
+      {
+        case "View employees":
+          employeeSearch();
+          break;
 
-      case "Find all artists who appear more than once":
-        multiSearch();
-        break;
+        case "Add employees":
+          employeeInsert();
+          break;
 
-      case "Find data within a specific range":
-        rangeSearch();
-        break;
+        case "Find data within a specific range":
+          rangeSearch();
+          break;
 
-      case "Search for a specific song":
-        songSearch();
-        break;
+        case "Search for a specific song":
+          songSearch();
+          break;
 
-      case "Find artists with a top song and top album in the same year":
-        songAndAlbumSearch();
-        break;
+        case "Find artists with a top song and top album in the same year":
+          songAndAlbumSearch();
+          break;
       }
     });
 }
 
-function artistSearch() {
+function employeeSearch() 
+{
   inquirer
-    .prompt({
-      name: "artist",
+    .prompt
+    ({
+      name: "employee",
       type: "input",
-      message: "What artist would you like to search for?"
+      message: "What employee would you like to search for?"
     })
-    .then(function(answer) {
-      var query = "SELECT position, song, year FROM top5000 WHERE ?";
-      connection.query(query, { artist: answer.artist }, function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-          console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+    .then(function(answer) 
+    {
+      var query = "SELECT * FROM employee WHERE ?";
+      connection.query(query, { employee: answer.employee }, function(err, res) 
+      {
+        if (err) throw err;
+
+        // Log all results of the SELECT statement
+        console.log(res);
+        runSearch();
+      });
+    });
+}
+
+function employeeInsert() 
+{
+  inquirer
+    .prompt
+    ({
+      name: "employee",
+      type: "input",
+      message: "What employee would you like to add?"
+    })
+    .then(function(answer) 
+    {  
+      var query = "INSERT INTO employee ?";
+      connection.query(query, {employee: answer.employee}, function(err, res) 
+      {
+        for (var i = 0; i < res.length; i++) 
+        {
+          console.log(res[i].artist);
         }
         runSearch();
       });
     });
 }
 
-function multiSearch() {
-  var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
-  connection.query(query, function(err, res) {
-    for (var i = 0; i < res.length; i++) {
-      console.log(res[i].artist);
-    }
-    runSearch();
-  });
-}
-
-function rangeSearch() {
+function rangeSearch() 
+{
   inquirer
-    .prompt([
+    .prompt
+    ([
       {
         name: "start",
         type: "input",
         message: "Enter starting position: ",
-        validate: function(value) {
-          if (isNaN(value) === false) {
+        validate: function(value) 
+        {
+          if (isNaN(value) === false) 
+          {
             return true;
           }
           return false;
@@ -105,19 +135,24 @@ function rangeSearch() {
         name: "end",
         type: "input",
         message: "Enter ending position: ",
-        validate: function(value) {
-          if (isNaN(value) === false) {
+        validate: function(value) 
+        {
+          if (isNaN(value) === false) 
+          {
             return true;
           }
           return false;
         }
       }
     ])
-    .then(function(answer) {
+    .then(function(answer)
+    {
       var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
       connection.query(query, [answer.start, answer.end], function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-          console.log(
+        for (var i = 0; i < res.length; i++) 
+        {
+          console.log
+          (
             "Position: " +
               res[i].position +
               " || Song: " +
@@ -133,17 +168,22 @@ function rangeSearch() {
     });
 }
 
-function songSearch() {
+function songSearch()
+{
   inquirer
-    .prompt({
+    .prompt
+    ({
       name: "song",
       type: "input",
       message: "What song would you like to look for?"
     })
-    .then(function(answer) {
+    .then(function(answer) 
+    {
       console.log(answer.song);
-      connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
-        console.log(
+      connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) 
+      {
+        console.log
+        (
           "Position: " +
             res[0].position +
             " || Song: " +
@@ -158,22 +198,28 @@ function songSearch() {
     });
 }
 
-function songAndAlbumSearch() {
+function songAndAlbumSearch() 
+{
   inquirer
-    .prompt({
+    .prompt
+    ({
       name: "artist",
       type: "input",
       message: "What artist would you like to search for?"
     })
-    .then(function(answer) {
+    .then(function(answer)
+    {
       var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
       query += "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
       query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
 
-      connection.query(query, [answer.artist, answer.artist], function(err, res) {
+      connection.query(query, [answer.artist, answer.artist], function(err, res) 
+      {
         console.log(res.length + " matches found!");
-        for (var i = 0; i < res.length; i++) {
-          console.log(
+        for (var i = 0; i < res.length; i++)
+        {
+          console.log
+          (
             i+1 + ".) " +
               "Year: " +
               res[i].year +
